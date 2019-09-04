92edf6a408b2353eae716f0d965df85fe85f56a2
Mikel Lopez <mikel.lopez@siag.es>
2019-09-04 10:15:11 +0200

    Initial Commit

---
+++ Viajero/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Viajero
+//
+//  Created by Mikel Lopez on 04/09/2019.
+//  Copyright © 2019 Mikel Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
